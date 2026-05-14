4083007173206ac8eb5e4337fdaf06bca2019d15
apolverari-dgsspa <alberto.polverari@dgsspa.com>
2026-05-14 10:39:06 +0200

    PN-19606: Implementato service di deanonimizzazione del CF

--- src/main/java/it/pagopa/pn/ioconnector/middleware/msclient/DataVaultClient.java
+++ src/main/java/it/pagopa/pn/ioconnector/middleware/msclient/DataVaultClient.java
@@ -2,9 +2,14 @@ package it.pagopa.pn.ioconnector.middleware.msclient;
 
 import it.pagopa.pn.commons.log.PnLogger;
 import it.pagopa.pn.ioconnector.generated.openapi.msclient.datavault.v1.api.RecipientsApi;
+import it.pagopa.pn.ioconnector.generated.openapi.msclient.datavault.v1.dto.BaseRecipientDto;
 import lombok.CustomLog;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @CustomLog
 @Component
@@ -13,8 +18,9 @@ public class DataVaultClient {
 
     private final RecipientsApi recipientsApi;
 
-    public String deanonymize(String internalId) {
+    public List<BaseRecipientDto> deanonymize(String internalId) {
         log.logInvokingExternalService(PnLogger.EXTERNAL_SERVICES.PN_DATA_VAULT, "recipientsApi");
-        return internalId;
+        List<String> internalIdParam = Collections.singletonList(internalId);
+        return recipientsApi.getRecipientDenominationByInternalId(internalIdParam);
     }
 }
--- src/main/java/it/pagopa/pn/ioconnector/service/DataVaultService.java
+++ src/main/java/it/pagopa/pn/ioconnector/service/DataVaultService.java
@@ -1,9 +1,12 @@
 package it.pagopa.pn.ioconnector.service;
 
+import it.pagopa.pn.ioconnector.generated.openapi.msclient.datavault.v1.dto.BaseRecipientDto;
 import it.pagopa.pn.ioconnector.middleware.msclient.DataVaultClient;
 import lombok.CustomLog;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @CustomLog
@@ -13,7 +16,11 @@ public class DataVaultService {
     private final DataVaultClient dataVaultClient;
 
     public String deanonymize(String internalId) {
-        // TODO: invocare il client
-        return internalId;
+        List<BaseRecipientDto> resp = dataVaultClient.deanonymize(internalId);
+        return resp.stream()
+                .filter(r -> internalId.equals(r.getInternalId()))
+                .map(BaseRecipientDto::getTaxId)
+                .findFirst()
+                .orElseThrow();
     }
 }
