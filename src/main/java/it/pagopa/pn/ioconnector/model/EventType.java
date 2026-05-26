aa316d681a3f1fc52c7eed1b35d38542473f5411
Alberto Polverari <alberto.polverari@dgsspa.com>
2026-05-26 13:54:32 +0200

    PN-19120: fix EventType

--- src/main/java/it/pagopa/pn/ioconnector/model/EventType.java
+++ src/main/java/it/pagopa/pn/ioconnector/model/EventType.java
@@ -9,7 +9,7 @@ public enum EventType {
 
     ACCEPTED(false),
     SENDER_NOT_ALLOWED(true),
-    SENT_TO_IO(false),
+    SENT_TO_IO(true),
     DELIVERED_TO_USER(true),
     READ(true),
     PAID(true),
