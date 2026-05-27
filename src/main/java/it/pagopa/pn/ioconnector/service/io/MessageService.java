e629a7aaa222562cc1ab7818548bc30a110d584c
Alberto Polverari <alberto.polverari@dgsspa.com>
2026-05-27 13:18:32 +0200

    PN-19620: fix post-merge

--- src/main/java/it/pagopa/pn/ioconnector/service/io/MessageService.java
+++ src/main/java/it/pagopa/pn/ioconnector/service/io/MessageService.java
@@ -22,6 +22,7 @@ import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
 import it.pagopa.pn.ioconnector.config.PnIoConnectorConfig;
+import it.pagopa.pn.ioconnector.generated.openapi.server.v1.dto.Attachment;
 import it.pagopa.pn.ioconnector.generated.openapi.server.v1.dto.MessageRequest;
 import it.pagopa.pn.ioconnector.generated.openapi.server.v1.dto.MessageResponse;
 import it.pagopa.pn.ioconnector.generated.openapi.server.v1.dto.PaymentData;
@@ -132,13 +133,16 @@ public class MessageService {
             && isSamePaymentData(request.getPaymentData(), entity.getPaymentData());
     }
 
-    private boolean isSameAttachments(List<String> requestAttachments,
+    private boolean isSameAttachments(List<Attachment> requestAttachments,
                                        List<IOConnectorRequestEntity.Attachment> entityAttachments) {
         if (requestAttachments == null && entityAttachments == null) return true;
         if (requestAttachments == null || entityAttachments == null) return false;
         if (requestAttachments.size() != entityAttachments.size()) return false;
         for (int i = 0; i < requestAttachments.size(); i++) {
-            if (!Objects.equals(requestAttachments.get(i), entityAttachments.get(i).getFileKey())) {
+            Attachment req = requestAttachments.get(i);
+            IOConnectorRequestEntity.Attachment ent = entityAttachments.get(i);
+            if (!Objects.equals(req.getId(), ent.getId())
+                    || !Objects.equals(req.getFileKey(), ent.getFileKey())) {
                 return false;
             }
         }
@@ -163,11 +167,6 @@ public class MessageService {
                 .subject(sqsMsg.getSubject())
                 .markdown(sqsMsg.getMarkdown())
                 .sensitiveContent(sqsMsg.getSensitiveContent())
-                .attachments(sqsMsg.getAttachments() != null
-                    ? sqsMsg.getAttachments().stream()
-                        .map(fk -> IOConnectorRequestEntity.Attachment.builder().fileKey(fk).build())
-                        .collect(Collectors.toList())
-                    : null)
                 .paymentData(sqsMsg.getPaymentData() != null
                         ? IOConnectorRequestEntity.PaymentData.builder()
                         .amount(sqsMsg.getPaymentData().getAmount())
